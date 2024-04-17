0e02205f75f61677eb04dd21a325e10b8f15ab88
Your Name <you@example.com>
2024-04-17 15:24:12 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// var filter = function (arr, fn) {
+//   const filterArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (fn(arr[i], i)) {
+//       filterArr.push(arr[i]);
+//     }
+//   }
+//   return filterArr;
+// };
+
+// function greaterThan10(n) {
+//   return n > 10;
+// }
+
+// const arr = [0, 10, 20, 30, 40, 50];
+
+// console.log(filter(arr, greaterThan10));
